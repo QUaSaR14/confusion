98ea8435890fda12b36ce00237f732587663ead5
QUaSaR <madhurtrivedi14@gmail.com>
2019-06-13 11:54:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent'
 // import './App.css';
@@ -13,20 +12,6 @@ function App() {
         </div>
       </Navbar>
       <Menu />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
