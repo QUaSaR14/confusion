0f305aca9ce517c71332c3b7674ac2411a033820
QUaSaR <madhurtrivedi14@gmail.com>
2019-06-18 14:10:12 +0530

    SPA 1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -32,22 +32,22 @@ class Header extends Component {
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home" >
-                                        <span className="fa fa-home fa-lg"></span>Home
+                                        <span className="fa fa-home fa-lg"></span> Home
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/aboutus" >
-                                        <span className="fa fa-info fa-lg"></span>About
+                                        <span className="fa fa-info fa-lg"></span> About
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/menu" >
-                                        <span className="fa fa-list fa-lg"></span>Menu
+                                        <span className="fa fa-list fa-lg"></span> Menu
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/contactus" >
-                                        <span className="fa fa-address-card fa-lg"></span>Contact
+                                        <span className="fa fa-address-card fa-lg"></span> Contact
                                     </NavLink>
                                 </NavItem>
                             </Nav>
