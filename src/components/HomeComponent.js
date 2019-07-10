b9d2b72818c3a6d7d6c59932a780c72d4590e69b
QUaSaR <madhurtrivedi14@gmail.com>
2019-07-10 17:13:21 +0530

    Assignment 2

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle, CardSubtitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle} from 'reactstrap';
 
-function RenderCard({ item }) {
-    return(
+function RenderCard({item}) {
+    return (
         <Card>
             <CardImg src={item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
                 <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
+
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from  '../shared/leaders';
@@ -57,6 +60,7 @@ class Main extends Component{
         <Header />
           <Switch> 
             <Route path="/home" component={HomePage} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
